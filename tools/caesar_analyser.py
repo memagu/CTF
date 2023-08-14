9f79ba849d3499a24b50402d2319ad0542c4fbd8
memagu <melker.widen@gmail.com>
2023-08-14 11:42:20 +0200

    Updated caesar cipher analyis tool.

--- tools/caesar_analyser.py
+++ tools/caesar_analyser.py
@@ -1,34 +1,69 @@
 #!/bin/python
 
+import argparse
 import string
+import sys
+from termcolor import colored
+from typing import Optional
+
+CHARCODE_A = 65
+CHARCODE_a = 97
+ALPHABET_LENGTH = 26
 
 
-upper = string.ascii_uppercase
-lower = string.ascii_lowercase
+def analyse_caesar_cipher(ciphertext: str, target: Optional[str]):
+    for rotation_offset in range(ALPHABET_LENGTH):
+        plaintext = ""
+        for char in ciphertext:
+            if char in string.ascii_uppercase:
+                plaintext += chr(((ord(char) - CHARCODE_A + rotation_offset) % ALPHABET_LENGTH) + CHARCODE_A)
+                continue
+    
+            if char in string.ascii_lowercase:
+                plaintext += chr(((ord(char) - CHARCODE_a + rotation_offset) % ALPHABET_LENGTH) + CHARCODE_a)
+                continue
+    
+            plaintext += char
 
-input_string = input("Enter string: ")
-target_string = input("Input target string: ")
-# s = "cvpbPGS{P7e1S_54I35_71Z3}"
-
-for i in range(0, 26):
-    out = ""
-    for char in input_string:
-        if char in upper:
-            out += chr(((ord(char) - 65 + i) % 26) + 65)
+        out = f"{rotation_offset: <6}{plaintext}"
+    
+        if target is not None and target in plaintext:
+            print(colored(out, "green"))
             continue
 
-        if char in lower:
-            out += chr(((ord(char) - 97 + i) % 26) + 97)
-            continue
+        print(out)
 
-        out += char
 
-    if target_string and target_string in out:
-        print(f"\033[92m{i}.\t{out}\033[0m")
-        continue
+def main():
+    argument_parser = argparse.ArgumentParser(
+        prog="Caesar Analyser",
+        description="A tool for analysing caesar ciphers."
+        )
+    argument_parser.add_argument(
+        "-t",
+        "--target-string",
+        type=str,
+        help="Specify a target string for easier identification of correct rotation offset."
+        )
+    argument_parser.add_argument(
+        "-i",
+        "--input-file",
+        type=argparse.FileType('r'),
+        default=sys.stdin,
+        help="Specify a input file."
+        )
 
-    print(f"{i}.\t{out}")
+    args = argument_parser.parse_args()
+    print(args)
 
+    analyse_caesar_cipher(args.input_file.read().strip(), args.target_string)
+
+    if args.input_file is not sys.stdin:
+        args.input_file.close()
+
+
+if __name__ == "__main__":
+    main()
 
 
 
